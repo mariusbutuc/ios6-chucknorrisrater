8fb192a3bdbc58d2ec91c1c402cd225e55ff0b1b
Marius Butuc <marius.butuc@gmail.com>
2013-07-07 20:21:17 -0400

    Initial Commit

---
+++ ChuckNorrisRater/IAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IAAppDelegate.h
+//  ChuckNorrisRater
+//
+//  Created by Marius Butuc on 13-07-07.
+//  Copyright (c) 2013 Marius Butuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChuckNorrisRater/IAViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAViewController.h
+//  ChuckNorrisRater
+//
+//  Created by Marius Butuc on 13-07-07.
+//  Copyright (c) 2013 Marius Butuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAViewController : UIViewController
+
+@end
