430f0afd88647d941b329bd632f2b1b1cfdd78ef
Marius Butuc <marius.butuc@gmail.com>
2013-07-07 22:04:41 -0400

    Understanding data serialization and interacting with external services

--- ChuckNorrisRater/IAViewController.m
+++ ChuckNorrisRater/IAViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "IAViewController.h"
+#import "IAHTTPCommunication.h"
 
 @interface IAViewController ()
+{
+    NSNumber *jokeID;
+}
+@property (weak, nonatomic) IBOutlet UILabel *jokeLabel;
 
 @end
 
@@ -17,13 +22,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self retrieveRandomJokes:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)retrieveRandomJokes:(id)sender
+{
+    IAHTTPCommunication *http = [[IAHTTPCommunication alloc] init];
+    NSURL *url = [NSURL URLWithString:@"http://api.icndb.com/jokes/random"];
+    [http retrieveURL:url successBlock:^(NSData *response)
+     {
+         NSError *error = nil;
+         NSDictionary *data = [NSJSONSerialization JSONObjectWithData:response
+                                                              options:0
+                                                                error:&error];
+         if (!error)
+         {
+             NSArray *entries = data[@"value"];
+             if (value && value[@"joke"])
+             {
+                 jokeID = value[@"id"];
+                 [self.jokeLabel setText:firstEntry[@"joke"]];
+             }
+         }
+     }];
+    
 }
 
 @end
